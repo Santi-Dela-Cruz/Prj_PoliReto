1800e547049cb7f2359293c425a18b3dbda4a256
CarlosFuentes25 <carlos.fuentes@epn.edu.ec>
2024-05-30 22:24:26 -0500

    Agregar clase FuentesCarlos y cambios en app

--- prj_PoliReto/src/App.java
+++ prj_PoliReto/src/App.java
@@ -5,7 +5,8 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         // Definicion de variables
-        char caracterUsuario;
+        int numeroFinalUsuario;
+        char caracterUsuario, caracterUsuarioDos;
         int numeroRepeticionesUsuario;
         char letraFinalUsuario;
         int nivelesFiguras;
@@ -13,12 +14,14 @@ public class App {
         // Instanciaminetos
         CuencaMarcos imprimirMCSC = new CuencaMarcos();
         DeLaCruzKevin DLK = new DeLaCruzKevin(1);
+        FuentesCarlos CF = new FuentesCarlos();
 
         // Ejecucion del codigo
         System.out.println("GRUPO 2");
         System.out.println("Integrantes del grupo:");
         System.out.println("Marcos Cuenca");
         System.out.println("De La Cruz Kevin");
+        System.out.println("Fuentes Carlos");
 
 
 
@@ -26,7 +29,23 @@ public class App {
 
         // Ingresar sus nombres por favor
 
+       
         System.out.println("----------------Ejercicios de la serie de numeros-----------------");
+        System.out.println("Para algunos de las siguientes series, se necesitara ingresar un numero que sera utilizado para el final de la serie ");
+        System.out.println("RECORDATORIO: si el numero ingresado es menor, la serie se adaptara al numero mas cercano");
+        System.out.println("Ingrese un numero que finalizara las series: ");
+        numeroFinalUsuario = Validadores.validarIngresoEnteros(sc);
+        System.out.print("SN8: ");
+        CF.CFSN8(numeroFinalUsuario);
+        System.out.println("");
+        System.out.print("SN9: ");
+        CF.CFSN9(numeroFinalUsuario);
+        System.out.println("");
+        System.out.print("SN10: ");
+        CF.CFSN10(numeroFinalUsuario);
+        System.out.println("");
+        System.out.print("SN12: ");
+        CF.CFSN12(numeroFinalUsuario);
         System.out.println("");
 
 
@@ -42,16 +61,20 @@ public class App {
         System.out.println("----------------Ejercicios de la serie de caracteres-----------------");
         System.out.println(
                 "Para algunos de estos ejercicios se necesitara un caracter a su eleccion, para otros una cantidad de repeticiones, y para otros la letra final para finalizar la serie");
-        System.out.println("Ingresa un caracter a tu eleccion");
+        System.out.println("Ingresa el primer caracter a tu eleccion");
         caracterUsuario = Validadores.validarIngresoCaracteres(sc);
+        System.out.println("Ingresa el segundo caracter a tu eleccion");
+        caracterUsuarioDos = Validadores.validarIngresoCaracteres(sc);
         System.out.println("Ingresa el numero de repeticiones que realizaran algunas series");
         numeroRepeticionesUsuario = Validadores.validarIngresoEnteros(sc);
         System.out.println("Ingresa la letra con la que finalizaran ciertas series");
         letraFinalUsuario = Validadores.validarIngresoLetra(sc);
 
         System.out.println("SC1: ");
+        CF.CFSC1(caracterUsuario, caracterUsuarioDos, numeroRepeticionesUsuario);
         System.out.println("");
         System.out.println("SC2: ");
+        CF.CFSC2(caracterUsuarioDos, numeroRepeticionesUsuario);
         System.out.println("");
 
         System.out.println("SC3: ");
---
+++ prj_PoliReto/src/FuentesCarlos.java
@@ -0,0 +1,110 @@
+
+/**
+ *Clase que realiza las series numericas del 8 al 12 y las series de caracteres 1 y 2
+ 
+ @author Fuentes Carlos
+ **/
+public class FuentesCarlos {
+    /** *
+     * Metodo que retorna una serie numerica, la cual finalizara en el numero ingresado por el usuario
+     * @param cfNumeroFinal
+    */
+    public void CFSN8(int cfNumeroFinal) {
+                
+        // Inicializar el primer número de la secuencia
+        int numero = 3;
+        
+        // Generar a secuencia 3, 8, 13, 18, 23...
+        //si el numero ingresado por el usuario es menor al ultimo numero, la serie se adaptara al numero mas cercano
+        while (numero <= cfNumeroFinal) {
+            System.out.print(numero + " ");
+            numero += 5;  
+        }
+       
+    }
+    /**
+     * Metodo que retorna una serie numerica, la cual finalizara en el numero ingresado por el usuario
+     * @param cfNumeroFinal
+     */
+    public void CFSN9(int cfNumeroFinal){
+
+        //inicializar el primer numero de la serie
+        int numeroDos = 2;
+        //Generar la serie 2, 4, 8, 16, 32...
+        //si el numero ingresado por el usuario es menor al ultimo numero, la serie se adaptara al numero mas cercano
+        while(numeroDos <= cfNumeroFinal){
+            System.out.print(numeroDos + " ");
+            numeroDos = numeroDos*2;
+        }
+
+    }
+    /**
+     * Metodo que retorna una serie numerica, la cual finalizara en el numero ingresado por el usuario
+     * @param cfNumeroFinal
+     */
+    public void CFSN10(int cfNumeroFinal){
+        //inicializar el primer numero
+        int numeroTres = 3;
+        //generar la serie 3, 9, 27, 81... 
+        while (numeroTres <= cfNumeroFinal) {
+            System.out.print(numeroTres + " ");
+            numeroTres = numeroTres*3;
+        }
+    }
+    /**
+     * Metodo que retorna una serie numerica, la cual finalizara en el numero ingresado por el usuario
+     * @param cfNumeroFinal
+     */
+    public void CFSN12(int cfNumeroFinal){
+        //inicializar el primer numero
+        int numeroCuatro = 2;
+        int contador = 4;
+        //generar la serie 2 6 12 20 30...
+        while (numeroCuatro <= cfNumeroFinal) {
+            
+            System.out.print(numeroCuatro + " ");
+            numeroCuatro += contador;
+            contador = contador+2;
+            
+        }
+    }
+    /**
+     * Este metodo retornara una serie de caracteres en patron secuencial
+     * @param cfCaracterUno
+     * @param cfCaracterDos
+     * @param cfNumeroRepeticione
+     */
+    public void CFSC1(char cfCaracterUno, char cfCaracterDos, int cfNumeroRepeticione){
+        //generara la seria CaracterUno, CaracterDos, CaracterUno, CaracterDos...
+        for(int i = 0; i < cfNumeroRepeticione;i++){
+            if(i%2==0){
+                System.out.print(cfCaracterUno + " ");
+            }else{
+                System.out.print(cfCaracterDos+ " ");
+            }
+        }
+    }
+
+    public void CFSC2(char cfCaracter, int cfNumeroRepeticiones){
+        int anterior = 1;
+        int actual = 1;
+        for (int i = 1; i <= cfNumeroRepeticiones; i++) {
+            for (int j = 1; j <= actual; j++) {
+                System.out.print(cfCaracter);
+            }
+            System.out.println();
+            if (actual > cfNumeroRepeticiones) {
+                break;
+            }
+            System.out.println();
+            int temp = actual;
+            actual = actual + anterior;
+            anterior = temp;
+
+        
+        }
+        
+    
+    
+    }
+}
